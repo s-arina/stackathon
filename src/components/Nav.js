9bd04103530af8fce3c0649af4bc64ca77b2f5ad
Sarina Chang <sarinachang7@gmail.com>
2022-05-08 04:20:20 -0400

    customizing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from 'react';
-// remember to import the hooks
 import '../css/Nav/Nav.css';
 
 function Nav(props) {
-  // react hook, set the show state default to be false
   const [show, handleShow] = useState(false);
 
-  // function to check if the scroll position is > 100,
-  // then handleShow will change the state to true
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
       handleShow(true);
@@ -16,27 +12,31 @@ function Nav(props) {
     }
   };
 
-  // attach an event listener as you scroll, it triggers the transitionNavBar function
   useEffect(() => {
     window.addEventListener('scroll', transitionNavBar);
-    // a return function to 'cleanup', when the component mounts, attach the function to the listener
-    // not necessary but good practice
     return () => window.removeEventListener('scroll', transitionNavBar);
   }, []);
 
   return (
-    // only render the nav_black background when the show is true and nav_black is true, otherwise the background is transparent
     <div className={`nav ${show && 'nav_black'}`}>
       <div className='nav_content'>
         <img className='nav_logo' src='/logo.png' alt='' />
         <div className='nav_links'>
-          <a href='/'>
+          <a href='/' className='link link--metis'>
             <p>Home</p>
           </a>
-          <p>TV Shows</p>
-          <p>Movies</p>
-          <p>New & Popular</p>
-          <p>My List</p>
+          <a href='/' className='link link--metis'>
+            <p>TV Shows</p>
+          </a>
+          <a href='/' className='link link--metis'>
+            <p>Movies</p>
+          </a>
+          <a href='/' className='link link--metis'>
+            <p>New & Popular</p>
+          </a>
+          <a href='/' className='link link--metis'>
+            <p>My List</p>
+          </a>
         </div>
         <img
           className='nav_avatar'
